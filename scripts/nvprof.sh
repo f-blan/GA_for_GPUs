8897dbd9e15cbd203202a3d0edf685926ef13a99
Francesco Blangiardi <c.blangio@gmail.com>
2021-12-18 04:49:26 +0100

    feat: in depth profiling for naive large case

--- scripts/nvprof.sh
+++ scripts/nvprof.sh
@@ -2,14 +2,13 @@
 
 rm a.out
 
-nvcc -isystem ./utils -isystem ./device_utils -isystem ./kernels ./main/$1.cu -lcurand  -o a.out 
+nvcc -isystem ./utils -isystem ./device_utils -isystem ./kernels -isystem ./main  ./main/$1.cu -lcurand  -o a.out 
 
 
 ./a.out
 
 mkdir logs/$2/$1_logs
-
-sudo /usr/local/cuda-10/bin/nvprof --log-file logs/$2/$1_logs/n.log  ./a.out 
+ 
 sudo /usr/local/cuda-10/bin/nvprof --print-gpu-trace --log-file logs/$2/$1_logs/gpu.log ./a.out 
 sudo /usr/local/cuda-10/bin/nvprof --print-api-trace --log-file logs/$2/$1_logs/api.log ./a.out
 sudo /usr/local/cuda-10/bin/nvprof --events all --log-file logs/$2/$1_logs/all.log ./a.out
